8b9701eced967d1e4ba91cd325840e021877d86a
FITSTARGILLY2643 <fitstargilii1194@gmail.com>
2021-06-05 19:04:51 +0300

    Define the general configuration file

--- config.py
+++ config.py
@@ -1,3 +1,12 @@
 # api_key='92024ca524b9462782a214ac25c41263'
 # BASE_URL='https://newsapi.org/v2/sources?apiKey=API_KEY'
 import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    NEWS_SOURCES_API_BASE_URL = 'https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+    SPECIFIC_SOURCE_API_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
+
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
