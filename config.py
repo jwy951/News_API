3d86f6bd0d29ee1759bb9ff1795a5d37c1226a70
FITSTARGILLY2643 <fitstargilii1194@gmail.com>
2021-06-05 19:07:00 +0300

    Define the development configuration class

--- config.py
+++ config.py
@@ -19,3 +19,18 @@ class ProdConfig(Config):
         Config: The parent configuration class with General configuration settings
     '''
     pass
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
