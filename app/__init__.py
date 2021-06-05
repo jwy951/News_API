bc58c01ae00537e51432485416d89a325d44b1f5
FITSTARGILLY2643 <fitstargilii1194@gmail.com>
2021-06-05 19:28:08 +0300

    Add create_app  initialization method

--- app/__init__.py
+++ app/__init__.py
@@ -4,3 +4,18 @@ from flask_bootstrap import Bootstrap
 
 # Initializing Flask Extensions
 bootstrap = Bootstrap()
+
+def create_app(config_name):
+
+    # Initializing application
+    app = Flask(__name__,instance_relative_config = True)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
